a60fbcbc7798d79fe89fcd937edb343c9535c6ca
xeeynamo@hotmail.com <xeeynamo@hotmail.com@c196104e-1c89-11de-b56b-c9554f25afe1>
2009-04-02 21:01:13 +0000

    Improved graphic, console implemented, added more movement with the directional keys

--- trunk/smallHex/Functions.h
+++ trunk/smallHex/Functions.h
@@ -6,7 +6,22 @@
 
 #define Key(i) GetAsyncKeyState(i)==-32767
 
+int32 p=0,size;
+
 int FileSize(FILE *file){
     fseek(file,0,SEEK_END);
     return ftell(file);
+}
+
+void Console(int t){
+    SetXY(2,24);
+    printf("Console: ");
+    switch (t){
+        case 0:
+            printf("Welcome to smallHex ^^");
+            break;
+        case 1:
+            printf("Offset: %08X/%08X",p,size);
+            break;
+    }
 }
--- trunk/smallHex/main.cpp
+++ trunk/smallHex/main.cpp
@@ -1,26 +1,37 @@
 #include "Functions.h"
 #define DEBUG
 
-int32 p=0;
 
 int main(){
+    SetConsoleTitle("smallHex");
+    SetCursorVisible(false);
+    SetBufferSizeY(25);
     byte *buf=(byte*)malloc(4096);
     FILE *file;
     char *path;
-    SetConsoleTitle("smallHex");
     #ifdef DEBUG
-        path="..\\smallhex\\test.bin";
+        path="test.bin";
         file=fopen("test.bin","r+b");
     #else
         printf("File to open: ");
         scanf("%s",path);
     #endif
     file=fopen(path,"r+b");
-WRITE:
+    size=FileSize(file);
+BEGIN:
     cls();
+    DrawLineX(1,GetBufferSizeY()-2,GetBufferSizeX()-2);
+    Console(0);
+WRITE:
+    Console(1);
     fseek(file,p,SEEK_SET);
     fread(buf,1,4096,file);
-    for(int i=0;i<720;i++) printf("%02X ",buf[i]);
+    for(int y=0;y<23;y++){
+        SetXY(1,y);
+        for(int x=0;x<26;x++){
+            printf("%02X ",buf[x+26*y]);
+        }
+    }
     while(1){
         Sleep(1);
 
@@ -30,6 +41,15 @@ WRITE:
         }
         else if (Key(VK_RIGHT)){
             p++;
+            goto WRITE;
+        }
+        else if (Key(VK_UP)){
+            if (p>26) p-=26;
+            else p=0;
+            goto WRITE;
+        }
+        else if (Key(VK_DOWN)){
+            p+=26;
             goto WRITE;
         }
         else if (Key(ESC))
