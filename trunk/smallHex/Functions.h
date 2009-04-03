888a6060f211b123b6325672a7db2863d6568ecb
xeeynamo@hotmail.com <xeeynamo@hotmail.com@c196104e-1c89-11de-b56b-c9554f25afe1>
2009-04-03 16:26:57 +0000

    Improved the "Hex/Char" and "Only Char" mode, added a red text for the first hex and char byte of the buffer and added the page scroll with PageUP and PageDown

--- trunk/smallHex/Functions.h
+++ trunk/smallHex/Functions.h
@@ -8,8 +8,23 @@
 
 int32 p=0,size;
 byte displaymode=0;
-byte bufX=19,bufY=23;
+byte bufX=19,bufY=23,
+     posXH=1,posXC=59;
 
+void Alias(register char Char){
+    switch(Char){
+        case 0x00: // NULL
+        case 0x07: // BEEP
+        case 0x08: // BACKSPACE
+        case 0x09: // TAB
+        case 0x0A: // NEW LINE
+        case 0x0D: // RETURN
+            printf(" ");
+            break;
+        default:
+            printf("%c",Char);
+    }
+}
 int FileSize(FILE *file){
     fseek(file,0,SEEK_END);
     return ftell(file);
